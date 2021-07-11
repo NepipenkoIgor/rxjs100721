b1605f27dee3a52fd2e07c6d78d00c294f351878
Igor Nepipenko <inepipenko@muellerone.com>
2021-07-11 11:55:30 +0300

    feat(no-ref) live search

--- 5-live-search/src/index.ts
+++ 5-live-search/src/index.ts
@@ -1,4 +1,23 @@
 import 'bootstrap';
 import '../../assets/css/style.css'
 import './styles.css'
+import { IRes, liveSearch, request } from './live-search';
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
+const el = document.querySelector('#search') as HTMLInputElement;
+const container = document.querySelector('.container') as HTMLDivElement;
+
+
+liveSearch(
+    fromEvent<InputEvent>(el, 'input'),
+    (text: string) => {
+        return request(ajax<IRes>({
+            url: `https://api.github.com/search/repositories?q=${text}`,
+            crossDomain: true
+        }))
+    }
+)
+    .subscribe((htmlStr) => {
+        container.innerHTML = htmlStr;
+    })
---
+++ 5-live-search/src/live-search.ts
@@ -0,0 +1,69 @@
+import {
+    bufferCount,
+    concatAll,
+    debounceTime,
+    distinctUntilChanged,
+    filter,
+    fromEvent,
+    map, Observable, reduce,
+    switchMap,
+    toArray
+} from 'rxjs';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
+export interface IResult {
+    name: string;
+    description: string;
+    owner: {
+        avatar_url: string
+    }
+}
+
+export interface IRes {
+    items: IResult[]
+}
+
+export function liveSearch(source1$: Observable<InputEvent>, source2Fn: (text: string) => Observable<string>) {
+    return source1$
+        .pipe(
+            debounceTime(300),
+            map((event: InputEvent) => {
+                return (event.target as HTMLInputElement).value.trim();
+            }),
+            filter((text: string) => text.length > 3),
+            distinctUntilChanged(),
+            switchMap(source2Fn));
+}
+
+export function request(source$: Observable<AjaxResponse<IRes>>): Observable<string> {
+    return  source$
+        .pipe(
+            map((res: AjaxResponse<IRes>) => {
+                return res.response.items;
+            }),
+            concatAll(),
+            map((item: IResult) => createCard(item)),
+            bufferCount(3),
+            reduce((resultStr: string, htmlStr: string[]) => {
+                return resultStr += createRow(htmlStr)
+            }, ''),
+            map((htmlStr: string) => htmlStr.trim().replace(/\s+(<)/g, '<'))
+        )
+}
+
+function createRow(htmlStr: string[]): string {
+    return `<div class="row">${htmlStr.join(' ')}</div>`;
+}
+
+function createCard({name, description, owner: {avatar_url}}: IResult): string {
+    return `
+      <div class="col-sm-6 col-md-4">
+        <div class="card">
+          <img class="card-img-top" src=${avatar_url} alt=${name}>
+          <div class="card-body">
+               <h5 class="card-title">${name}</h5>
+               <p class="card-text">${description}</p>
+          </div>
+        </div> 
+     </div>  
+    `
+}
